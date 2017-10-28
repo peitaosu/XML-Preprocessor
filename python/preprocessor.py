4c0f91b037c9410f067497cb7641a015cef1d839
Tony Su <peitaosu@163.com>
2017-10-28 23:26:41 +0800

    read original content as list

--- python/preprocessor.py
+++ python/preprocessor.py
@@ -7,19 +7,19 @@ class Preprocessor():
         self.original_file["file"] = file_path
         try:
             with open(file_path, "r") as original_file:
-                self.original_file["content"] = original_file.read()
+                self.original_file["content"] = original_file.read().split("\n")
             return 0
         except:
             return -1
 
     def preprocess(self):
-        pass
+        self.processed_file["content"] = self.original_file["content"]
 
     def save(self, file_path):
         self.processed_file["file"] = file_path
         try:
             with open(file_path, "w") as processed_file:
-                processed_file.write(self.processed_file["content"])
+                processed_file.write("\n".join(self.processed_file["content"]))
             return 0
         except:
             return -1
