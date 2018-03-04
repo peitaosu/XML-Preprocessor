f8c962bf52fdbfe6a5f13379969d7a46efa87e5c
Tony Su <peitaosu@163.com>
2018-03-04 22:42:22 +0800

    implement the main function

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -3,13 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace XMLPreprocessor
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            string inXml;
+            string outXml = "";
+            if (args.Length > 0)
+            {
+                inXml = args[0];
+                if (args.Length == 2)
+                {
+                    outXml = args[1];
+                }
+            }
+            else
+            {
+                Console.WriteLine("XMLPreprocessor.exe in.xml [out.xml]");
+                return -1;
+            }
+            
+            
+            Preprocessor preprocessor = new Preprocessor();
+            XmlDocument processedXmlDoc = new XmlDocument();
+            processedXmlDoc = preprocessor.Process(inXml, null);
+            if(outXml == "")
+            {
+                processedXmlDoc.Save(inXml);
+            }
+            else
+            {
+                processedXmlDoc.Save(outXml);
+            }
+            return 0;
         }
     }
 }
